cbf059c25fa511ef6a9345387cffc9ba6cb84cba
Lucas Tito <lucassga500@gmail.com>
2025-08-07 21:55:55 -0300

    fix: put validation messages back to work
    there's also a new validation for service list code when services is selected in item type

---
+++ Challenge_Fambec.Client/Mocks/MockWithException.cs
@@ -0,0 +1,17 @@
+namespace Challenge_Fambec.Client.Mocks;
+
+public static class MockWithException
+{
+    public static readonly string[] NcmsWithException = [
+        "17049000",
+        "22030000", 
+        "24029000",
+        "87032310",
+        "73269099",
+        "84713012",
+        "85171231",
+        "90189099",
+        "39269099",
+        "73239300"
+    ];
+}
